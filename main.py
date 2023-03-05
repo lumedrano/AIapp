dc3d1a700cc177706bde23639bcf634345a2bf47
Luigi Medrano <luigimedrano03@gmail.com>
2023-03-05 10:28:20 -0600

    app

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.image("https://imageio.forbes.com/specials-images/imageserve/5ee2978b434cc00006720d80/0x0.jpg?format=jpg&width=1200")
+st.title("AI Model - Luigi & Alex")
+
+st.text_input("Enter Prompt: ")
+
