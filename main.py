49ea077ba823a8f600c3e9ebf3652773ecd3e0d9
Luigi Medrano <luigimedrano03@gmail.com>
2023-03-05 17:30:03 -0600

    formatting edit

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import streamlit as st
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, pipeline
+
+# tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+# model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 
-st.image("https://imageio.forbes.com/specials-images/imageserve/5ee2978b434cc00006720d80/0x0.jpg?format=jpg&width=1200")
-st.title("AI Model - Luigi & Alex")
+# st.image("https://imageio.forbes.com/specials-images/imageserve/5ee2978b434cc00006720d80/0x0.jpg?format=jpg&width=1200")
+# st.title("AI Model - Luigi & Alex")
 
-st.text_input("Enter Prompt: ")
+# st.text_input("Enter Prompt: ")
 
+
+# Load the text generation pipeline
+generator = pipeline('text-generation', model='gpt2')
+
+# Generate text
+text = generator('Hello, how are you?', max_length=50)[0]['generated_text']
+
+# Display the generated text on Streamlit
+st.write(text)
