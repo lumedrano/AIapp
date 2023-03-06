08b4d72af34d9677f574ec891d22920569ae52a5
Luigi Medrano <luigimedrano03@gmail.com>
2023-03-05 18:06:18 -0600

    example model

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import streamlit as st
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, pipeline
 
-# tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-# model = GPT2LMHeadModel.from_pretrained('gpt2')
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 
-# st.image("https://imageio.forbes.com/specials-images/imageserve/5ee2978b434cc00006720d80/0x0.jpg?format=jpg&width=1200")
-# st.title("AI Model - Luigi & Alex")
+st.image("https://imageio.forbes.com/specials-images/imageserve/5ee2978b434cc00006720d80/0x0.jpg?format=jpg&width=1200")
+st.title("AI Model - Luigi & Alex")
 
-# st.text_input("Enter Prompt: ")
+def generated_text(user_input):
+    input_ids = tokenizer.encode(user_input, return_tensors='pt')
+    
+    output = model.generate(input_ids, 
+                            max_length=150, 
+                            temperature=0.6,
+                            repetition_penalty=3.0,
+                            top_k=50,
+                            top_p=0.75,
+                            do_sample=True  )
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    return generated_text
 
 
+user_input = st.text_input("Enter Prompt: ")
 
-# Define an event listener that updates the cursor position
-# def update_cursor_position():
-#     st.session_state.cursor_position = st.get_last_cursor_position()
+if st.button("Send"):
+    st.write(generated_text(user_input))
 
-# # Create a text input field and add the event listener
-# text_input = st.text_input('Type something here:', '', on_change=update_cursor_position)
-
-# # Display the text input and the current cursor position
-# st.write(f'You typed: {text_input}, Cursor position: {st.session_state.cursor_position}')
-
-
-# Create a text input field
-text_input = st.text_input('Type something here:', '')
-
-# Display the text input and the current cursor position
-st.write(f'You typed: {text_input}, Cursor position: {st.session_state.cursor_position}')
