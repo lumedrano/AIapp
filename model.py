388551184855a4a1e7f1f9830644e9f92d363cdf
Luigi Medrano <luigimedrano03@gmail.com>
2023-03-18 09:21:50 -0500

    model bin

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+#from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch
 from transformers import BertForQuestionAnswering, BertTokenizer
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2LMHeadModel.from_pretrained('gpt2')
+# tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+# model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 
 bert_tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
@@ -13,18 +13,18 @@ bert_model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-
 # st.image("https://imageio.forbes.com/specials-images/imageserve/5ee2978b434cc00006720d80/0x0.jpg?format=jpg&width=1200")
 # st.title("AI Model - Luigi & Alex")
 
-def generated_text(user_input):
-    input_ids = tokenizer.encode(user_input, return_tensors='pt')
+# def generated_text(user_input):
+#     input_ids = tokenizer.encode(user_input, return_tensors='pt')
     
-    output = model.generate(input_ids, 
-                            max_length=150, 
-                            temperature=0.6,
-                            repetition_penalty=3.0,
-                            top_k=50,
-                            top_p=0.75,
-                            do_sample=True)
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-    return generated_text
+#     output = model.generate(input_ids, 
+#                             max_length=150, 
+#                             temperature=0.6,
+#                             repetition_penalty=3.0,
+#                             top_k=50,
+#                             top_p=0.75,
+#                             do_sample=True)
+#     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+#     return generated_text
 
 
 
